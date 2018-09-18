92fa12f904021e9ba9cd14ea1e4467e67b50a4c7
benny-wang <benny.wang@surrey.ca>
2018-09-17 18:09:43 -0700

    base circle ci set up

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
+docker rmi galileo-node raiblocks-node -f
+
 docker kill $(docker ps -q)
 
 git submodule update --init --recursive
 
 docker/node/build.sh live
 
-docker run -d -p 7011:7011/udp -p 7011:7011 -p 7012:7012 -v ${pwd}:/root raiblocks-node:latest
+docker run -d -p 7011:7011/udp -p 7011:7011 -p 7012:7012 -v ${pwd}:/root galileo-node:latest
 
 # curl -d '{"action": "block_count"}' localhost:7076
