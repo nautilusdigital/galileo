e999f9486d53a74700f3c26814d131dbdcaa8881
benny-wang <benny.wang@surrey.ca>
2018-10-22 16:48:43 -0700

    net network

--- rai/lib/numbers.cpp
+++ rai/lib/numbers.cpp
@@ -59,7 +59,7 @@ void rai::uint256_union::encode_account (std::string & destination_a) const
 		number_l >>= 5;
 		destination_a.push_back (account_encode (r));
 	}
-	destination_a.append ("_brx"); // xrb_
+	destination_a.append ("_lag"); // xrb_
 	std::reverse (destination_a.begin (), destination_a.end ());
 }
 
